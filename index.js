8b43f8c28f0c21487e7a679b73cedc22082b9e3b
Reece Crook <reececrook16@gmail.com>
2023-04-06 17:02:48 -0600

    Completed Lab

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Add your code here
+const url = 'http://localhost:3000/users'
+const body = document.querySelector('body')
+function submitData(name, email){
+    const contentData = {
+        name: `${name}`, 
+        email: `${email}`
+    };
+    return fetch('http://localhost:3000/users', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json',
+        },
+        body: JSON.stringify(contentData),
+        
+    })
+    .then(response => response.json())
+    .then(object => body.append(JSON.stringify(object)))
+    .catch(error => {
+        alert("Oh No");
+        body.append(error.message)
+        
+    })
+}
+
+
+//submitData('reece', 't')
